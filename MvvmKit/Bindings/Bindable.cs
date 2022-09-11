e4f7b6fb7f5e4e034a96877bb5aff45e4a7f0d9e
Diego Stamigni <diego.stamigni@gmail.com>
2022-09-11 10:04:22 +0100

    NotifyPropertyChanged classic Set<> added

--- MvvmKit/Bindings/Bindable.cs
+++ MvvmKit/Bindings/Bindable.cs
@@ -30,6 +30,19 @@ public abstract class Bindable : INotifyPropertyChanged
 		OnPropertyChanged(name!);
 	}
 
+	protected bool Set<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+	{
+		if (EqualityComparer<T>.Default.Equals(field, value))
+		{
+			return false;
+		}
+
+		field = value;
+		OnPropertyChanged(propertyName);
+
+		return true;
+	}
+
 	protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
 		=> this.PropertyChanged?.Invoke(this, new(propertyName));
 }
